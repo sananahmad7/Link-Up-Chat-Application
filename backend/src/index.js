8f98d87f095cbf1c61792a1265d06d8463f88be6
Sanan <sananahmad909@gmail.com>
2025-02-14 22:58:51 +0500

    Sign Up page made. Functionality working

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
+import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import cookieParser from "cookie-parser";
@@ -10,6 +10,12 @@ app.use(cookieParser());
 
 dotenv.config();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div>LoginInPage</div>
+    )
+}
+
+export default LoginPage;
