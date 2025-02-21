eb1f45618ce80fd5a827d0c9b619050258d8a23c
Sanan <sananahmad909@gmail.com>
2025-02-21 16:56:02 +0500

    project prepared for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import { app, server } from "./libs/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import { connectDB } from "./libs/db.js";
 import cookieParser from "cookie-parser";
 app.use(cookieParser());
 
 dotenv.config();
+const Port = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(
   cors({
@@ -18,8 +21,13 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const Port = process.env.PORT;
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //Middleware to allow to extract json data
 
